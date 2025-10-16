db075015e6d6f41088c5a7b0ddadd00959547b13
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-16 20:16:47 +0700

    Menambahkan teks "My Music" pada fungsi IniLatetakBoxColumnRow

--- app/src/main/java/com/example/tataletak/Tataletak.kt
+++ app/src/main/java/com/example/tataletak/Tataletak.kt
@@ -71,3 +71,14 @@ fun IniLatetakBoxColumnRow(modifier: Modifier = Modifier) {
                 contentDescription = null,
                 contentScale = ContentScale.Fit
             )
+            Text(
+                text = "My Music",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
+}
